d8d1c525463eea3239668a58098706f95e9c02dd
Rohan2596 <rohankadam662@gmail.com>
2020-12-31 15:50:39 +0530

    WIP:Dashboard work in progress.

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,10 +1,11 @@
 import  React,{Component}  from "react";
+import { DashboardHeader } from "../components/dash_header";
 
 export class Dashboard extends Component{
   render(){
       return(
           <>
-          <span>dsfs</span>
+         <DashboardHeader/>
 
           </>
       );
